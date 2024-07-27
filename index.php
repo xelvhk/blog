752d3c8a783e951b5a051a1b732aeb0fb978aaff
Lex <khlex93@gmail.com>
2024-07-27 15:59:57 +0300

    added redirect and error info

--- index.php
+++ index.php
@@ -11,37 +11,47 @@ $stmt = $pdo->query(
     ORDER BY
         created_at DESC'
 );
-if ($stmt === false) {
+if ($stmt === false)
+{
     throw new Exception('There was a problem running this query');
 }
+
+$notFound = isset($_GET['not-found']);
 
 ?>
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>A blog application</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    </head>
+    <body>
+        <?php require 'templates/title.php' ?>
 
-<head>
-    <title>My blog application</title>
-    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-</head>
+        <?php if ($notFound): ?>
+            <div style="border: 1px solid #ff6666; padding: 6px;">
+                Error: cannot find the requested blog post
+            </div>
+        <?php endif ?>
 
-<body>
-    <h1>Blog title</h1>
-    <p>This paragraph summarises what the blog is about.</p>
-    <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
-        <h2>
-            <?php echo htmlEscape($row['title']) ?>
-        </h2>
-        <div>
-            <?php echo convertSqlDate($row['created_at']) ?>
-            (<?php echo countCommentsForPost($row['id']) ?> comments)
-        </div>
-        <p>
-            <?php echo htmlEscape($row['body']) ?>
-        </p>
-        <p>
-            <a href="view-post.php?post_id=<?php echo $row['id'] ?>">Read more...</a>
-        </p>
-    <?php endwhile ?>
-</body>
+        <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
+            <h2>
+                <?php echo htmlEscape($row['title']) ?>
+            </h2>
+            <div>
+                <?php echo convertSqlDate($row['created_at']) ?>
+                
+                (<?php echo countCommentsForPost($row['id']) ?> comments)
+            </div>
+            <p>
+                <?php echo htmlEscape($row['body']) ?>
+            </p>
+            <p>
+                <a
+                    href="view-post.php?post_id=<?php echo $row['id'] ?>"
+                >Read more...</a>
+            </p>
+        <?php endwhile ?>
 
+    </body>
 </html>
