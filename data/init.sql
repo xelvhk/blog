0ca23942e02c9d6238e6fd396ed65a91921ae6b5
Lex <khlex93@gmail.com>
2024-08-12 23:14:10 +0300

    added htaccess

--- data/init.sql
+++ data/init.sql
@@ -1,3 +1,28 @@
+* Database creation script
+ */
+/* Foreign key constraints need to be explicitly enabled in SQLite */
+PRAGMA foreign_keys = ON;
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    created_at VARCHAR NOT NULL,
+    is_enabled BOOLEAN NOT NULL DEFAULT true
+);
+/* This will become user = 1. I'm creating this just to satisfy constraints here.
+   The password will be properly hashed in the installer */
+INSERT INTO
+    user
+    (
+        username, password, created_at, is_enabled
+    )
+    VALUES
+    (
+        "admin", "unhashed-password", datetime('now', '-3 months'), 0
+    )
+;
+
 DROP TABLE IF EXISTS post;
 CREATE TABLE post (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -5,7 +30,8 @@ CREATE TABLE post (
     body VARCHAR NOT NULL,
     user_id INTEGER NOT NULL,
     created_at VARCHAR NOT NULL,
-    updated_at VARCHAR
+    updated_at VARCHAR,
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
 INSERT INTO
     post
@@ -53,7 +79,8 @@ CREATE TABLE comment (
     created_at VARCHAR NOT NULL,
     name VARCHAR NOT NULL,
     website VARCHAR,
-    text VARCHAR NOT NULL
+    text VARCHAR NOT NULL,
+    FOREIGN KEY (post_id) REFERENCES post(id)
 );
 INSERT INTO
     comment
@@ -81,10 +108,3 @@ INSERT INTO
         "This is a comment from Alex"
     )
 ;
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    username VARCHAR NOT NULL,
-    password VARCHAR NOT NULL,
-    created_at VARCHAR NOT NULL,
-    is_enabled BOOLEAN NOT NULL DEFAULT true
-);
