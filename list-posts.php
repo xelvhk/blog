9f97b8d7774086f6d8e428bf29d74669c607399c
Lex <khlex93@gmail.com>
2024-08-18 22:54:08 +0300

    added creation time for posts and post counter

--- list-posts.php
+++ list-posts.php
@@ -1,12 +1,30 @@
 <?php
 require_once 'lib/common.php';
+require_once 'lib/list-posts.php';
 session_start();
 // Don't let non-auth users see this screen
 if (!isLoggedIn())
 {
     redirectAndExit('index.php');
 }
+if ($_POST)
+{
+    $deleteResponse = $_POST['delete-post'];
+    if ($deleteResponse)
+    {
+        $keys = array_keys($deleteResponse);
+        $deletePostId = $keys[0];
+        if ($deletePostId)
+        {
+            deletePost(getPDO(), $deletePostId);
+            redirectAndExit('list-posts.php');
+        }
+    }
+}
 
+// Connect to the database, run a query
+$pdo = getPDO();
+$posts = getAllPosts($pdo);
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,48 +35,31 @@ if (!isLoggedIn())
     <body>
         <?php require 'templates/top-menu.php' ?>
         <h1>Post list</h1>
+        <p>You have <?php echo count($posts) ?> posts.
         <form method="post">
             <table id="post-list">
                 <tbody>
-                    <tr>
-                        <td>Title of the first post</td>
-                        <td>
-                            <a href="edit-post.php?post_id=1">Edit</a>
-                        </td>
-                        <td>
-                            <input
-                                type="submit"
-                                name="post[1]"
-                                value="Delete"
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Title of the second post</td>
-                        <td>
-                            <a href="edit-post.php?post_id=2">Edit</a>
-                        </td>
-                        <td>
-                            <input
-                                type="submit"
-                                name="post[2]"
-                                value="Delete"
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Title of the third post</td>
-                        <td>
-                            <a href="edit-post.php?post_id=3">Edit</a>
-                        </td>
-                        <td>
-                            <input
-                                type="submit"
-                                name="post[3]"
-                                value="Delete"
-                            />
-                        </td>
-                    </tr>
+                <?php foreach ($posts as $post): ?>
+                        <tr>
+                            <td>
+                                <?php echo htmlEscape($post['title']) ?>
+                            </td>
+                            <td>
+                                <?php echo convertSqlDate($post['created_at']) ?>
+                            </td>
+                            <td>
+                                <a href="edit-post.php?post_id=<?php echo $post['id']?>">Edit</a>
+                            </td>
+                            <td>
+                                <input
+                                    type="submit"
+                                    name="delete-post[<?php echo $post['id']?>]"
+                                    value="Delete"
+                                />
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+
                 </tbody>
             </table>
         </form>
