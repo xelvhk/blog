5d5c7fd9850730d1c068ffb4f59771d388cb4a90
Lex <khlex93@gmail.com>
2024-08-18 19:35:53 +0300

    fixed login page display

--- edit-post.php
+++ edit-post.php
@@ -71,7 +71,12 @@ if ($_POST)
         <?php require 'templates/head.php' ?>
     </head>
     <body>
-        <?php require 'templates/title.php' ?>
+    <?php require 'templates/top-menu.php' ?>
+        <?php if (isset($_GET['post_id'])): ?>
+            <h1>Edit post</h1>
+        <?php else: ?>
+            <h1>New post</h1>
+        <?php endif ?>
         <?php if ($errors): ?>
             <div class="error box">
                 <ul>
@@ -106,6 +111,7 @@ if ($_POST)
                     type="submit"
                     value="Save post"
                 />
+                <a href="index.php">Cancel</a>
             </div>
         </form>
     </body>
